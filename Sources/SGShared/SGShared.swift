81a5a583610c814814cf9556bf490d7f59b04452
Sietse Guijt <smguijt@outlook.com>
2024-06-09 22:13:11 +0200

    .

--- Sources/SGShared/SGShared.swift
+++ Sources/SGShared/SGShared.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public struct SGShared {
+public struct SGShared: Codable {
     
     public private(set) var text = "SGShared Library"
     
