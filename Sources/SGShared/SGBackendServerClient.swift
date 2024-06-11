c03d2275c18004594a90088f6f0c3f845727d925
Sietse Guijt <smguijt@outlook.com>
2024-06-11 19:54:30 +0200

    .

--- Sources/SGShared/SGBackendServerClient.swift
+++ Sources/SGShared/SGBackendServerClient.swift
@@ -1,8 +1,35 @@
 //
-//  File.swift
-//  
+//  SGBackendServerClient.swift
+//
 //
 //  Created by Sietse Guijt on 11/06/2024.
 //
 
 import Foundation
+
+public struct SGBackendServerClient {
+    
+    public private(set) var clientPort: String
+    public private(set) var clientUrl: String
+    public private(set) var baseUrl: URL
+    
+    public init(clientUrl: String = "http://127.0.0.1", clientPort: String = "8080") {
+        self.clientPort = clientPort
+        self.clientUrl = clientUrl
+        self.baseUrl = URL(string: "")!
+    }
+    
+    @available(macOS 12.0, *)
+    func fetchData(endPoint: String) async throws -> String {
+        let url = self.baseUrl.appendingPathComponent(endPoint)
+        let (data, _) = try await URLSession.shared.data(for: URLRequest(url: url))
+        guard let responseBody = String(data: data, encoding: .utf8) else {
+            throw Errors.invalidResponseEncoding
+        }
+        return responseBody
+    }
+
+    enum Errors: Error {
+        case invalidResponseEncoding
+    }
+}
--- Sources/SGShared/SGShared.swift
+++ Sources/SGShared/SGShared.swift
@@ -1,10 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import Foundation
+
 public struct SGShared: Decodable {
     
     public private(set) var text = "SGShared Library"
-    
-    public init() {
-    }
+
 }
